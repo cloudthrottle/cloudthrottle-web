df565635b771a5c421c6c325d5d89f8fb5f8cffd
David <davie668@gmail.com>
2022-02-23 08:11:52 +0000

    Removes console log

--- src/repositories/locos/buildFunctionButtons.ts
+++ src/repositories/locos/buildFunctionButtons.ts
@@ -2,7 +2,6 @@ import {PartialFunctionButtons} from "../../types";
 import {FunctionButtonKind, FunctionButtons} from "@cloudthrottle/dcc-ex--commands";
 
 export const buildFunctionButtons = (buttons: PartialFunctionButtons | undefined = {}): FunctionButtons => {
-    console.log("buildButtons", buttons);
     return Array(29).fill(null)
         .reduce((acc, currentValue, currentIndex) => {
             const buttonData = buttons[currentIndex]
